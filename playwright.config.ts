308e88ebdead2560ab5ca56f63cfb809cf3291b5
Sanjiv Kadam <sanjivkadam39@gmail.com>
2026-02-21 12:29:54 +0400

    Fullsccreen 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
   testDir: './tests/session4',
   /* Run tests in files in parallel */
   fullyParallel: true,
+  
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -28,6 +29,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     viewport: null,
+     headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
